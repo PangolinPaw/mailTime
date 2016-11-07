b8b9d4444d904bc58f114c1622478057783c61a0
PangolinPaw <PangolinPaw@users.noreply.github.com>
2016-11-07 20:35:06 +0000

    Create index.php

---
+++ index.php
@@ -0,0 +1,21 @@
+<html>
+<head>
+	<title>mailTime</title>
+
+	<link rel="stylesheet" href="styles/main.css">
+	<link href="https://fonts.googleapis.com/css?family=Ubuntu" rel="stylesheet">
+
+</head>
+
+<body>
+	<div class="container">
+		<div class="header">
+			<h1>mailTime</h1>
+			<div>Info</div>
+		</div>
+		<?php
+			echo '<p>PHP version: ' . phpversion() . '</p>';
+		?>
+	</div>
+</body>
+</html>
